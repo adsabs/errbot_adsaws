91f9edd47d313db8704b1d93ce2091dc731da90c
Jonny Elliott <jonnynelliott@googlemail.com>
2015-10-04 17:46:07 -0400

    Should now pass python3 v2

--- tests/unittests/test_adsaws.py
+++ tests/unittests/test_adsaws.py
@@ -19,13 +19,10 @@ class TestAdsAws(unittest.TestCase):
         # Add the EC2 instances for mocking
         ec2_connection = boto3.resource('ec2', region_name='us-east-1')
         rc1 = ec2_connection.create_instances(ImageId='ami-0022c769', MinCount=1, MaxCount=1)
-        rc2 = ec2_connection.create_instances(ImageId='ami-005db969', MinCount=1, MaxCount=1)
         ec2_connection.create_tags(Resources=[rc1[0].id], Tags=[{'Key': 'Name', 'Value': 'test1'}])
-        ec2_connection.create_tags(Resources=[rc2[0].id], Tags=[{'Key': 'Name', 'Value': 'test2'}])
 
         expected_ec2 = [
             {'status': 'running', 'tag': 'test1'},
-            {'status': 'running', 'tag': 'test2'}
         ]
 
         running_ec2 = get_ec2_running()
