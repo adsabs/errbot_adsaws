5906fc85c4acef2e91c3548e9f4708b802a7df45
Edwin Henneken <ehenneken@cfa.harvard.edu>
2016-10-12 15:03:59 -0400

    bibgroup comparison basic modules

--- adsaws_config.py
+++ adsaws_config.py
@@ -7,6 +7,10 @@ AWS_REGION = 'us-east-1'
 AWS_ACCESS_KEY = 'key'
 AWS_SECRET_KEY = 'secret'
 
+DATA_DIR = '/data/links'
+
+API_KEY = 'very secret key'
+
 try:
     from local_config import *
 except:
